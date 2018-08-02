b39cad7296c395f26f7f7f9be0dfdfee7cea46a9
“Tyler <tylerpalef@gmail.com>
2018-08-02 10:53:17 -0400

    added guy fieri to spice things up

--- server.rb
+++ server.rb
@@ -5,17 +5,21 @@ get '/' do
 end
 
 get '/about' do
+ @title = 'About'
  erb :about
 end
 
 get '/contact' do
+  @title = 'Contact'
   erb :contact
 end
 
 get '/legal' do
+  @title = 'Legal'
   erb :legal
 end
 
 get '/recipes' do
+  @title = 'Recipes'
   erb :recipes
 end
