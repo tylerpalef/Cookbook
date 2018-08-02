99333c35e08e932157e29c33e698ce9dc8024b5e
“Tyler <tylerpalef@gmail.com>
2018-08-02 12:25:43 -0400

    fixed active record... still an error in my server.rb and such but it runs

--- recipe.rb
+++ recipe.rb
@@ -1,20 +1,16 @@
-gem 'activerecord', '=5.2.0'
-require 'active_record'
+gem 'activerecord', '=4.2.10'
+ require 'active_record'
 require 'mini_record'
-
-ActiveRecord::Base.establish_connection(
+ ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'cookbook.sqlite3'
 )
-
-class Recipe < ActiveRecord::Base
+ class Recipe < ActiveRecord::Base
   field :name, as: :string
   field :prep_time, as: :integer
   field :description, as: :text
-  # Text has a higher char limit than string
 end
-
-Recipe.auto_upgrade!
-at_exit do
+ Recipe.auto_upgrade!
+ at_exit do
   ActiveRecord::Base.connection.close
 end
